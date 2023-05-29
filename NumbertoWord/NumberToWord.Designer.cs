adca714e97fa7498b46b65995ef327737e1c1f5c
WHITE CODER <whitecooder@gmail.com>
2023-05-29 22:17:56 +0500

    Add project files.

---
+++ NumbertoWord/NumberToWord.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace NumbertoWord
+{
+    partial class NumberToWord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDigit = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkOrange;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(569, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(569, 100);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CONVERT NUMBER TO WORD";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtDigit
+            // 
+            this.txtDigit.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDigit.Location = new System.Drawing.Point(12, 116);
+            this.txtDigit.Name = "txtDigit";
+            this.txtDigit.Size = new System.Drawing.Size(429, 29);
+            this.txtDigit.TabIndex = 1;
+            this.txtDigit.TextChanged += new System.EventHandler(this.txtDigit_TextChanged);
+            this.txtDigit.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDigit_KeyPress);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(447, 116);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(110, 29);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtWord
+            // 
+            this.txtWord.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWord.Location = new System.Drawing.Point(12, 151);
+            this.txtWord.Multiline = true;
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(545, 203);
+            this.txtWord.TabIndex = 3;
+            // 
+            // NumberToWord
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 375);
+            this.Controls.Add(this.txtWord);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtDigit);
+            this.Controls.Add(this.panel1);
+            this.Name = "NumberToWord";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Number to Word Converter";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDigit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtWord;
+    }
+}
+
---
+++ NumbertoWord/NumberToWord.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumbertoWord
+{
+    public partial class NumberToWord : Form
+    {
+        public NumberToWord()
+        {
+            InitializeComponent();
+        }
+
+        private void txtDigit_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtDigit.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only number...", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtDigit.Text = txtDigit.Text.Remove(txtDigit.Text.Length - 1);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtDigit.Clear();
+            txtDigit.Focus();
+            txtWord.Clear();
+        }
+
+        public static string NumberToWords(int number)
+        {
+            if (number == 0) return "Zero";
+            if (number < 0) return "minus" + NumberToWords(Math.Abs(number));
+            string words = "";
+
+            if((number / 1000000) > 0)
+            {
+                words += NumberToWords(number / 1000000) + " Million ";
+                number %= 1000000;
+            }
+            if((number / 1000) > 0)
+            {
+                words += NumberToWords(number / 1000) + " Thousand ";
+                number %= 1000;
+            }
+            if((number / 100)> 0)
+            {
+                words += NumberToWords(number / 100) + " Hundred ";
+                number %= 100;
+            }
+            if(number > 0)
+            {
+                var unitmap = new[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven",
+                    "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+                var tensmap = new[] { "Zero", "Ten", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
+                if (number < 20)
+                {
+                    words += unitmap[number];
+                }
+                else
+                {
+                    words += tensmap[number / 10];
+                    if((number % 10) > 0)
+                    {
+                        words += "-" + unitmap[number % 10];
+                    }
+                }
+            }
+
+            return words;
+
+            
+        }
+
+        private void txtDigit_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(txtDigit.Text.Trim() != string.Empty)
+            {
+                if(e.KeyChar == Convert.ToChar(Keys.Enter))
+                {
+                    try
+                    {
+                        txtWord.Text = NumberToWords(Convert.ToInt32(txtDigit.Text));
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Please Check your Amount.", "Error");
+                    }
+                }
+                else if(txtDigit.Text.Trim() == string.Empty)
+                {
+                    txtWord.Clear();
+                }
+            }
+        }
+    }
+}
